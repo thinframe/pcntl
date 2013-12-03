8c739984bd1c8354e01394daba3744cf6e94b30f
Sorin Badea <sorin.badea@softvision.ro>
2013-12-03 10:54:51 +0200

    Application

---
+++ src/ThinFrame/Pcntl/PcntlApplication.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * /vendor/thinframe/pcntl/src/ThinFrame/Pcntl/PcntlApplication.php
+ *
+ * @copyright 2013 Sorin Badea <sorin.badea91@gmail.com>
+ * @license   MIT license (see the license file in the root directory)
+ */
+
+namespace ThinFrame\Pcntl;
+
+use ThinFrame\Applications\AbstractApplication;
+use ThinFrame\Applications\DependencyInjection\ContainerConfigurator;
+use ThinFrame\Events\EventsApplication;
+
+/**
+ * Class PcntlApplication
+ *
+ * @package ThinFrame\Pcntl
+ * @since   0.1
+ */
+class PcntlApplication extends AbstractApplication
+{
+    /**
+     * initialize configurator
+     *
+     * @param ContainerConfigurator $configurator
+     *
+     * @return mixed
+     */
+    public function initializeConfigurator(ContainerConfigurator $configurator)
+    {
+        // noop
+    }
+
+    /**
+     * Get configuration files
+     *
+     * @return mixed
+     */
+    public function getConfigurationFiles()
+    {
+        return [
+
+        ];
+    }
+
+    /**
+     * Get application name
+     *
+     * @return string
+     */
+    public function getApplicationName()
+    {
+        return 'PcntlApplication';
+    }
+
+    /**
+     * Get parent applications
+     *
+     * @return AbstractApplication[]
+     */
+    protected function getParentApplications()
+    {
+        return [
+            new EventsApplication()
+        ];
+    }
+}
